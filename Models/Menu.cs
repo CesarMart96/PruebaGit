2f2c0aaf122a392de68ef0c741c0204d63d5c3aa
CesarABM <chepiro1996@gmail.com>
2022-07-06 15:19:15 -0500

    nada mas

--- Models/Menu.cs
+++ Models/Menu.cs
@@ -17,7 +17,9 @@ namespace TheCapitalBurguer.Models
         [StringLength(100)]
         public string Nombre { get; set; }
 
-        public string Ingredientes { get; set; }
+        public string Ingrediente { get; set; }
+
+        public string Tamaño { get; set; }
         [Column(TypeName = "decimal(6, 2)")]
         public decimal? Precio { get; set; }
     }
